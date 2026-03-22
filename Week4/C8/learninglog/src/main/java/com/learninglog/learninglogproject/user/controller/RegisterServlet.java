5b52c0fcb32352fb36caeb2cc261d60e91bebf4e
ishankfl <ishankafle5@gmail.com>
2026-03-22 09:44:19 +0545

    c8-tutorial-done

---
+++ Week4/C8/learninglog/src/main/java/com/learninglog/learninglogproject/user/controller/RegisterServlet.java
@@ -0,0 +1,19 @@
+package com.learninglog.learninglogproject.user.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("pages/register.jsp");
+        rd.forward(req,resp);
+    }
+}
