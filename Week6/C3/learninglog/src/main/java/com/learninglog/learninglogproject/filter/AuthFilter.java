b7d0c553db7bdbc66e91ea2166a24d9c2cfab962
ishankfl <ishankafle5@gmail.com>
2026-04-19 12:10:32 +0545

    filter-added

---
+++ Week6/C3/learninglog/src/main/java/com/learninglog/learninglogproject/filter/AuthFilter.java
@@ -0,0 +1,37 @@
+package com.learninglog.learninglogproject.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebFilter("/*") //
+public class AuthFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException{
+
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String uri = req.getRequestURI();
+
+        // “Give me the existing session if it already exists, otherwise return null.”
+        HttpSession session = req.getSession(false);
+
+        boolean loggedIn = (session != null && session.getAttribute("user") != null);
+        // true
+
+        boolean isAuthPage = uri.contains("login") || uri.contains("register");
+        // false
+
+        if (loggedIn || isAuthPage ) {
+            chain.doFilter(request, response);
+
+        } else {
+            res.sendRedirect(req.getContextPath() + "/login");
+        }
+
+    }
+}
